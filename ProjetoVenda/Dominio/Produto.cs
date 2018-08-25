9baabad484754cf1ca549fd277b2bd5990468983
Jackson Silva <cajui54@gmail.com>
2018-08-25 14:43:59 -0300

    Projeto iniciado - implementação da classe Produto

---
+++ ProjetoVenda/Dominio/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoVenda.Dominio
+{
+    class Produto
+    {
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+        public double preco { get; set; }
+
+        public Produto(int codigo, string descricao, double preco)
+        {
+            this.codigo = codigo;
+            this.descricao = descricao;
+            this.preco = preco;
+        }
+        public override string ToString()
+        {
+            return "Codigo: "+ codigo +" ,Descrição: "+ descricao +" ,Preço R$: "+preco.ToString("F2");
+        }
+
+    }
+}
---
+++ ProjetoVenda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoVenda.Dominio;
+
+namespace ProjetoVenda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto P = new Produto(1001, "Cadeira simples", 500);
+            Console.WriteLine(P);
+
+            Console.ReadKey();
+        }
+    }
+}
