9ab169ae760a94c2cd94725edb752a1e80e4df54
Jackson Silva <cajui54@gmail.com>
2018-08-26 14:11:05 -0300

    Implementação da classe Tela responsável pela interação com o usuário

--- ProjetoVenda/Program.cs
+++ ProjetoVenda/Program.cs
@@ -9,10 +9,48 @@ namespace ProjetoVenda
 {
     class Program
     {
+        public static List<Produto> produtos = new List<Dominio.Produto>();
         static void Main(string[] args)
         {
-            Produto P = new Produto(1001, "Cadeira simples", 500);
-            Console.WriteLine(P);
+            int opcao = 0;
+            produtos.Add(new Produto(1001, "Cadeira simples", 500));
+            produtos.Add(new Produto(1002, "Cadeira acolchoada", 900));
+            produtos.Add(new Produto(1003, "Sofá de três lugares", 2000 ));
+            produtos.Add(new Produto(1004,"Mesa retangular",1500));
+            produtos.Add(new Produto(1005, "Mesa retangular", 2000));
+
+            while(opcao != 5)
+            {
+                Console.Clear();
+                Tela.mostrarMenu();
+                try
+                {
+                    opcao = int.Parse(Console.ReadLine());
+                }catch(Exception erro)
+                {
+                    Console.WriteLine("Erro inesperado !"+ erro);
+                    opcao = 0;
+                }
+                switch (opcao)
+                {
+                    case 1:
+                        Tela.mostrarProdutos();
+                         break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Console.WriteLine("Fim do Programa!");
+                        break;
+                    default :
+                        Console.WriteLine("Opção inválida ["+ opcao +"]");
+                        break;
+                }
+
+            }
 
             Console.ReadKey();
         }
--- ProjetoVenda/Tela.cs
+++ ProjetoVenda/Tela.cs
@@ -8,5 +8,32 @@ namespace ProjetoVenda
 {
     class Tela
     {
+        //Classe responsável por interagir com o usuário
+
+        public static void mostrarMenu()
+        {
+             Console.WriteLine("1 - Listar produtos ordenadamente");
+             Console.WriteLine("2 - Cadastrar produto");
+             Console.WriteLine("3 - Cadastrar pedido");
+             Console.WriteLine("4 - Mostrar dados de um pedido");
+            Console.WriteLine("5 - Sair");
+            Console.Write("Opção: ");
+
+        }
+        public static void mostrarProdutos()
+        {
+            Console.WriteLine();
+            Console.WriteLine("LISTAGEM DE PRODUTOS: ");
+            for(int i = 0; i < Program.produtos.Count; i++)
+            {
+
+                Console.WriteLine(Program.produtos[i]);
+
+            }
+            Console.ReadLine();
+
+        }
+
+
     }
 }
